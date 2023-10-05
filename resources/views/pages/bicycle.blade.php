9f1b97781f0980a5b421019003236abd849fdab0
Moontys <mantas.stirpeika@gmail.com>
2023-10-06 02:14:25 +0300

    Updated

--- resources/views/pages/bicycle.blade.php
+++ resources/views/pages/bicycle.blade.php
@@ -10,11 +10,9 @@
             <div class="well">
                 <h1><a href="/bicycle/{{$product->id}}">{{$product->bicycle_brand}}</a></h1>
                 <h1>{{$product->bicycle_price}}</h1>
-                <h1>{{$product->bicycle_description}}</h1>
+                <h3>{{$product->bicycle_description}}</h3>
+                <hr>
+                <h5>{{$product->created_at}}</h5>
             </div>
     </div>
-
-    <br><br><br><br><br><br>
-
-    <h3><a href="{{URL::to('/all_bicycles')}}">ALL bicycles in the basment</a></h3>
 @endsection
